ffb4b6e74ae53f856b7003c552811c30f5280ebc
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-04-30 16:56:49 +0200

    intégration front-page

---
+++ front-page.php
@@ -0,0 +1,68 @@
+<?php get_header();?>
+
+<section class="last-news">
+	<div class="container">
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="https://placehold.it/1280x450" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>First slide label</h5>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="https://placehold.it/1280x450" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Second slide label</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="https://placehold.it/1280x450" class="d-block w-100" alt="...">
+					<div class="carousel-caption d-none d-md-block">
+						<h5>Third slide label</h5>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+</section>
+<section class="container pt-5">
+    <div class="row">
+        <div class="col-md-4"><img src="https://placehold.it/250x150" class="d-block w-100" alt="...">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci enim ipsam maiores nulla odit.</p>
+        </div>
+        <div class="col-md-4"><img src="https://placehold.it/250x150" class="d-block w-100" alt="...">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci alias cumque eius rem vero.</p>
+        </div>
+        <div class="col-md-4"><img src="https://placehold.it/250x150" class="d-block w-100" alt="...">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eveniet iure molestias tempore tenetur veritatis.</p>
+        </div>
+    </div>
+</section>
+<section class="informations">
+    <h2 class="text-center">Informations</h2>
+</section>
+<section class="container">
+    <div class="row">
+        <div class="col-md-6"><img src="https://placehold.it/350x350" alt=""></div>
+        <div class="col-md-6"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate magnam maxime optio quaerat ratione reprehenderit!</p></div>
+    </div>
+</section>
+
+<?php get_footer();?>
